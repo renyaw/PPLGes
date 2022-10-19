64c1a2c289e75f2d4e15f983113f68b8c9f75a5d
reikochn <norarenada@students.undip.ac.id>
2022-10-19 20:41:49 +0700

    ganti login

--- login.php
+++ login.php
@@ -5,43 +5,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-night.min.css" rel="stylesheet"> -->
+
+    <style>
+      body {
+        width: auto;
+        height: 100vh;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-image: url("img/undiplnscp.png");
+      }
+    </style>
   </head>
   <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    
     <?php
     //session_start();
     require_once('db_login.php');
     ?>
-    
-    <div class="container text-center">
-      <br />
-      <br />
-      <img src="img/logo.png" alt="Logo" width="100" height="115" class="d-inline-block" />
-      <br />
-      <h3><b>informatika</b></h3>
-    </div>
-    <br />
-    <div class="container col-5">
-      <div class="form-control">
-        <br />
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <div class="container h-100">
+      <div class="row align-items-center h-100">
+        <div class="col-6 mx-auto">
+          <div class="card p-4 rounded-0 shadow-lg">
+            <div class="container text-center">
+              <img src="img/logo.png" alt="Logo" width="100" height="115" class="d-inline-block" />
+              <br />
+              <br />
+              <h3><b>Informatika</b></h3>
+            </div>
+            <br />
+            <div class="container">
+              <div class="form-control bg-light rounded-0">
+                <br />
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" />
+                </div>
+                <div class="mb-3 form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                  <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                </div>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                  <a href="srs10.html"><button class="btn btn-primary">Login</button></a>
+                </div>
+                <br />
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" />
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">Remember me</label>
-        </div>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-          <a href="srs10.html"><button class="btn btn-primary">Login</button></a>
-        </div>
-        <br />
       </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
