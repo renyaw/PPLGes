da85d1317fcac9640a2edd80f44bf4aa3695bbfb
Fathan <74785057+Churma16@users.noreply.github.com>
2022-10-19 21:39:57 +0700

    cek_login

--- login.php
+++ login.php
@@ -21,7 +21,7 @@
   </head>
   <body>
     <?php
-    //session_start();
+
     require_once('db_login.php');
     ?>
     <div class="container h-100">
@@ -38,21 +38,23 @@
             <div class="container">
               <div class="form-control bg-light rounded-0">
                 <br />
-                <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                </div>
-                <div class="mb-3">
-                  <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" />
-                </div>
-                <div class="mb-3 form-check">
-                  <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                  <label class="form-check-label" for="exampleCheck1">Remember me</label>
-                </div>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                  <a href="srs10.html"><button class="btn btn-primary">Login</button></a>
-                </div>
+                <form name="form" method="POST" autocomplete="off" action="cek_login.php">
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Username</label>
+                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" />
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password"/>
+                  </div>
+                  <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                    <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                  </div>
+                  <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <input type="submit" name="submit" id="submit" value="submit" />
+                  </div>
+                </form>
                 <br />
               </div>
             </div>
