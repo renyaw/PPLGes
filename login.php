6a65b38b297b41652f30ac96a224c080b95ef753
reikochn <norarenada@students.undip.ac.id>
2022-10-19 16:49:57 +0700

    nambah dblogin

--- login.php
+++ login.php
@@ -8,6 +8,12 @@
   </head>
   <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    
+    <?php
+    //session_start();
+    require_once('db_login.php');
+    ?>
+    
     <div class="container text-center">
       <br />
       <br />
