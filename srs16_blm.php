34d8ff69b24f0e29f4f7adef57e3f67c28250ac2
Fathan <74785057+Churma16@users.noreply.github.com>
2022-10-20 21:50:15 +0700

    Update srs16_blm.php

--- srs16_blm.php
+++ srs16_blm.php
@@ -8,6 +8,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
   </head>
   <body>
+    <?php
+    session_start();
+    require_once('db_login.php');
+    
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <include src="navdep.html"></include>
     <br />
@@ -23,6 +28,12 @@
       </div>
       <br />
       <div class="d-grid gap-2 d-md-block">
+        <?php
+          $query = $db->query("SELECT angkatan FROM mahasiswa where nim in(SELECT max(nim) FROM mahasiswa group by angkatan) order by angkatan");
+          while($row=$query->fetch_object()){
+            echo "<button class='btn btn-outline-secondary mx-1' id='angkatan' value='$row->angkatan' onclick='showSkripsi($row->angkatan);showSkripsibelum($row->angkatan);showHeader($row->angkatan);'>".$row->angkatan."</button>";
+          }
+        ?>
         <button class="btn btn-outline-secondary" type="button" disabled>2017</button>
         <button class="btn btn-secondary" type="button">2018</button>
         <button class="btn btn-outline-secondary" type="button" disabled>2019</button>
