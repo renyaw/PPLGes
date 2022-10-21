d264c9ab713109e6c44006b0427a682c39bb7dc8
imadearya <imadearya1@gmail.com>
2022-10-21 10:42:17 +0700

    navdep

--- srs12.php
+++ srs12.php
@@ -7,7 +7,8 @@
     <script src="https://unpkg.com/htmlincludejs"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
   </head>
-  <body class="body">
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <include src="navdep.html"></include>
     <?php
     session_start();
