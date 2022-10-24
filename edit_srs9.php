2b5bffed46903caac6f01d210e775ac3b8783ffb
reikochn <norarenada@students.undip.ac.id>
2022-10-24 22:12:05 +0700

    srs 9 lagi lagi, kaliini editny

--- edit_srs9.php
+++ edit_srs9.php
@@ -1,151 +1,107 @@
 <?php
 require_once "db_login.php";
-//mengambil file yang dikirim
+//get data from url
 $id = $_GET["id"];
 
-//mengambil data query yang dipilih
-$getdata = $db->query(
-  "SELECT mahasiswa.nim, mahasiswa.nama, mahasiswa.email, khs.smt, khs.status FROM mahasiswa,khs WHERE nim='$id'"
-);
+// //mengambil data query yang dipilih
+// $getdata = $db->query(
+//   "SELECT mahasiswa.nim, mahasiswa.nama, mahasiswa.email, khs.smt, khs.status FROM mahasiswa,khs WHERE nim='$id'"
+// );
 
-$data = mysqli_fetch_assoc($getdata);
-//mengisi data pada variabel
-$nim= $data['nim'];
-$nama = $data["nama"];
-$email = $data["email"];
-$status = $data["status"];
-
-
-//mengecek belum apa susah menonton submit
-if (!isset($_POST["submit"])) {
-    $query = "SELECT mahasiswa.nim, mahasiswa.nama, mahasiswa.email, khs.smt, khs.status FROM mahasiswa,khs WHERE nim='$id'" . $id;
-    //execute the query
-    $result = $db->query($query);
-    if (!$result) {
-        die("Could not the query database: <br />" . $db->error);
-    } else {
-        while ($row = $result->fetch_object()) {
-            $name = $row->status;
-        }
-    }
-} else {
-    $valid = true; //flag validasi
-    $status = test_input($_POST["status"]);
-    if ($status == "") {
-        $error_status = "Name is required";
-        $valid = false;
-    }
+// $data = mysqli_fetch_assoc($getdata);
+// //mengisi data pada variabel
+// $nim= $data['nim'];
+// $nama = $data["nama"];
+// $email = $data["email"];
+// $status = $data["status"];
 
 
-    //update data into database
-    if ($valid) {
-        //asign a query
-        $query = "UPDATE pesanan SET status='" .$status ."'WHERE id_pesanan=" .$id;
-        if($status=='2'){
-          $query2= $db->query("UPDATE ruang SET status = '0' WHERE no_ruang='$no_ruang'");
-        }
-        else{
-          $query3= $db->query("UPDATE ruang SET status = '1' WHERE no_ruang='$no_ruang'");
-        }
-        //execute the query
-        $result = $db->query($query);
-        if (!$result) {
-            die(
-                "Could not the query the database: <br />" .
-                    $db->error .
-                    "<br>Query:" .
-                    $query
-            );
-        } else {
-            //ketika sudah di submit , maka akan langsung pindah ke halaman view_customer.php
-            $db->close();
-            header("Location: dashboard_admin.php");
-        }
-    }
-}
+// //mengecek belum apa susah menonton submit
+// if (!isset($_POST["submit"])) {
+//     $query = "SELECT mahasiswa.nim, mahasiswa.nama, mahasiswa.email, khs.smt, khs.status FROM mahasiswa,khs WHERE nim='$id'" . $id;
+//     //execute the query
+//     $result = $db->query($query);
+//     if (!$result) {
+//         die("Could not the query database: <br />" . $db->error);
+//     } else {
+//         while ($row = $result->fetch_object()) {
+//             $name = $row->status;
+//         }
+//     }
+// } else {
+//     $valid = true; //flag validasi
+//     $status = test_input($_POST["status"]);
+//     if ($status == "") {
+//         $error_status = "Name is required";
+//         $valid = false;
+//     }
+
+
+//     //update data into database
+//     if ($valid) {
+//         //asign a query
+//         $query = "UPDATE pesanan SET status='" .$status ."'WHERE id_pesanan=" .$id;
+//         if($status=='2'){
+//           $query2= $db->query("UPDATE ruang SET status = '0' WHERE no_ruang='$no_ruang'");
+//         }
+//         else{
+//           $query3= $db->query("UPDATE ruang SET status = '1' WHERE no_ruang='$no_ruang'");
+//         }
+//         //execute the query
+//         $result = $db->query($query);
+//         if (!$result) {
+//             die(
+//                 "Could not the query the database: <br />" .
+//                     $db->error .
+//                     "<br>Query:" .
+//                     $query
+//             );
+//         } else {
+//             //ketika sudah di submit , maka akan langsung pindah ke halaman view_customer.php
+//             $db->close();
+//             header("Location: dashboard_admin.php");
+//         }
+//     }
+// }
 ?>
 
-<div class="container pt-5">
-        <div class="card">
-            <div class="card-header">Edit Data Mahasiswa</div>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard Operator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
+    <script src="https://unpkg.com/htmlincludejs"></script>
+  </head>
+  <body>
+    <div class="container">
+        <div class="card mt-5">
+            <h5 class="card-header">Edit Data Mahasiswa</h1>
             <div class="card-body">
-              <div class="row">
-                <div class="col-6">
-                <div class="form-group">
-                    <h5><label class="fw-1" for="id_pesanan">ID Pesanan:</label></h5>
-                    <p><?php echo $id_pesanan; ?></p>
+              <form action="" method="get">
+                <div class="mb-3 row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+                    </div>
                 </div>
-                <div class="form-group">
-                    <h5><label class="fw-1" for="name">Nama Pemesan:</label></h5>
-                    <p><?php echo $nama; ?></p>
-                </div>
-                <div class="form-group">
-                    <h5><label class="fw-1" for="name">Email:</label></h5>
-                    <p><?php echo $email; ?></p>
-                </div>
-                </div>
-                <div class="col-6">
-                <div class="form-group">
-                    <h5><label class="fw-1" for="tipe">Room Type:</label></h5>
-                    <p><?php echo $tipe; ?></p>
-                </div>
-                <div class="form-group">
-                    <h5><label class="fw-1" for="noruang">Nomor Ruangan:</label></h5>
-                    <p><?php echo $no_ruang; ?></p>
-                </div>
-                <div class="form-group">
-                    <h5><label class="fw-1" for="name">Status Pesanan:</label></h5>
-                    <?php if ($status == 0) {
-                        echo "<p>" . "Pesanan Anda Belum dikonfirmasi" . "</p>";
-                    } elseif ($status == 1) {
-                        echo "<p>" . "Pesanan Anda Sudah dikonfirmasi" . "</p>";
-                    } elseif ($status == 2) {
-                        echo "<p>" . "Pesanan Anda Dibatalkan" . "</p>";
-                    } ?>
-                </div>
-                </div>
-              </div>
-                <form method="POST" autocomplete="on" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ."?id=" .$id; ?>">
-                  <div class="form-group">
-                      <h5><label class="fw-1" for="status">Ubah Status:</label></h5>
-                      <select name="status" id="status" class="form-control" required>
-                          <option value="none" <?php if (isset($status)) {
-                              echo 'selected="true"';
-                          } ?>>--Select a status--</option>
-
-                          <option value="0" <?php if (
-                              isset($status) &&
-                              $status == "0"
-                          ) {
-                              echo 'selected="true"';
-                          } ?>>Belum Dikonfirmasi</option>
-
-                          <option value="1" <?php if (
-                              isset($status) &&
-                              $status == "1"
-                          ) {
-                              echo 'selected="true"';
-                          } ?>>Sudah Dikonfirmasi</option>
-
-                          <option value="2" <?php if (
-                              isset($status) &&
-                              $status == "2"
-                          ) {
-                              echo 'selected="true"';
-                          } ?>>Ditolak</option>
-                      </select>
-                      <div class="text-danger"><?php if (isset($error_status)) {
-                          echo $error_status;
-                      } ?></div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                   </div>
-                  <br>
-                  <button type="submit" class="btn btn-primary" name="submit" value="submit" >Submit</button>
-                  <a href="dashboard_admin.php" class="btn btn-secondary">Cancel</a>
-                </form>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                  </div>
+                  <button type="submit" class="btn btn-outline-success">Submit</button>
+              </form>
             </div>
         </div>
     </div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script src="ajaxsrs9.js"></script>
   </body>
-  <?php $db->close(); ?>
 </html>
