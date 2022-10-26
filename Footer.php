d3fcc70e8f041153ada74dd1e0e193414d74112f
Fathan <74785057+Churma16@users.noreply.github.com>
2022-10-26 22:49:41 +0700

    Update Footer.php

--- Footer.php
+++ Footer.php
@@ -10,7 +10,7 @@
 <link rel="stylesheet" href="./Footer_files/A.fonts,,_icomoon,,_style.css+css,,_bootstrap.min.css+css,,_style.css,Mcc.WMhAfb3NNP.css.pagespeed.cf.x0dnF8r-lD.css">
 
 
-<title>Footer 03</title>
+<title>footer</title>
 <script defer="" referrerpolicy="origin" src="./Footer_files/s.js.download"></script><script nonce="e8128599-0747-4ac6-9b06-dfce2af399d4">(function(w,d){!function(Z,_,ba,bb){Z.zarazData=Z.zarazData||{};Z.zarazData.executed=[];Z.zaraz={deferred:[],listeners:[]};Z.zaraz.q=[];Z.zaraz._f=function(bc){return function(){var bd=Array.prototype.slice.call(arguments);Z.zaraz.q.push({m:bc,a:bd})}};for(const be of["track","set","debug"])Z.zaraz[be]=Z.zaraz._f(be);Z.zaraz.init=()=>{var bf=_.getElementsByTagName(bb)[0],bg=_.createElement(bb),bh=_.getElementsByTagName("title")[0];bh&&(Z.zarazData.t=_.getElementsByTagName("title")[0].text);Z.zarazData.x=Math.random();Z.zarazData.w=Z.screen.width;Z.zarazData.h=Z.screen.height;Z.zarazData.j=Z.innerHeight;Z.zarazData.e=Z.innerWidth;Z.zarazData.l=Z.location.href;Z.zarazData.r=_.referrer;Z.zarazData.k=Z.screen.colorDepth;Z.zarazData.n=_.characterSet;Z.zarazData.o=(new Date).getTimezoneOffset();Z.zarazData.q=[];for(;Z.zaraz.q.length;){const bl=Z.zaraz.q.shift();Z.zarazData.q.push(bl)}bg.defer=!0;for(const bm of[localStorage,sessionStorage])Object.keys(bm||{}).filter((bo=>bo.startsWith("_zaraz_"))).forEach((bn=>{try{Z.zarazData["z_"+bn.slice(7)]=JSON.parse(bm.getItem(bn))}catch{Z.zarazData["z_"+bn.slice(7)]=bm.getItem(bn)}}));bg.referrerPolicy="origin";bg.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(Z.zarazData)));bf.parentNode.insertBefore(bg,bf)};["complete","interactive"].includes(_.readyState)?zaraz.init():Z.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,0,"script");})(window,document);</script><style type="text/css">@font-face { font-family: Roboto; src: url("chrome-extension://mcgbeeipkmelnpldkobichboakdfaeon/css/Roboto-Regular.ttf"); }</style></head>
 <body>
 </div>
@@ -19,7 +19,7 @@
 <div class="row mb-5">
 <div class="col-md-4">
 <div class="site-logo">
-<a href="https://preview.colorlib.com/theme/bft/bootstrap-footer-03/#">Colorlib</a>
+<a href="https://preview.colorlib.com/theme/bft/bootstrap-footer-03/#">Informatika</a>
 </div>
 </div>
 <div class="col-md-8 text-md-right">
