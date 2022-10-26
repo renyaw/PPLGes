3e311630abefaf730b6f1698873eba0be2c1167a
felliiptr <79779482+felliiptr@users.noreply.github.com>
2022-10-26 23:31:38 +0700

    fotoprofile

--- ppl.sql
+++ ppl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2022 at 07:53 PM
+-- Generation Time: Oct 26, 2022 at 06:30 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -115,6 +115,7 @@ INSERT INTO `khs` (`id_khs`, `smt`, `status`, `status_konfirmasi`, `ip_semester`
 CREATE TABLE `mahasiswa` (
   `nim` varchar(14) NOT NULL,
   `nama` varchar(50) NOT NULL,
+  `fotoprofile` varchar(100) DEFAULT NULL,
   `angkatan` varchar(4) NOT NULL,
   `alamat` varchar(50) NOT NULL,
   `nomor_telp` varchar(15) NOT NULL,
@@ -128,12 +129,12 @@ CREATE TABLE `mahasiswa` (
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`nim`, `nama`, `angkatan`, `alamat`, `nomor_telp`, `email`, `jalur_masuk`, `kode_kab`, `kode_wali`) VALUES
-('24060120130049', 'Abdul Mustajir', '2021', 'Komplek Tugu Timur', '087888327118', 'Mustajir@gmail.com', 'SBMPTN', '12', 'E1'),
-('24060120130050', 'Salma Nora Renada', '2020', 'Jalan Kenangan Indah', '0812347892138', 'sumbulnada@gmail.com', 'SNMPTN', '13', 'E1'),
-('24060120130053', 'Fathan Muhammad Faqih', '2020', 'Komplek Tugu', '087888327118', 'fathan@gmail.com', 'SBMPTN', '12', 'E1'),
-('24060120130059', 'Fathan Muhammad Rohmat', '2021', 'Komplek Tugu Barat', '087888327118', 'abdul@gmail.com', 'SNMPTN', '12', 'E1'),
-('24060120130152', 'Made Rohmat Subarjo', '2019', 'Komplek Jayabaya', '081315487465', 'made@gmail.com', 'SBMPTN', '13', 'E1');
+INSERT INTO `mahasiswa` (`nim`, `nama`, `fotoprofile`, `angkatan`, `alamat`, `nomor_telp`, `email`, `jalur_masuk`, `kode_kab`, `kode_wali`) VALUES
+('24060120130049', 'Abdul Mustajir', NULL, '2021', 'Komplek Tugu Timur', '087888327118', 'Mustajir@gmail.com', 'SBMPTN', '12', 'E1'),
+('24060120130050', 'Salma Nora Renada', NULL, '2020', 'Jalan Kenangan Indah', '0812347892138', 'sumbulnada@gmail.com', 'SNMPTN', '13', 'E1'),
+('24060120130053', 'Fathan Muhammad Faqih', NULL, '2020', 'Komplek Tugu', '087888327118', 'fathan@gmail.com', 'SBMPTN', '12', 'E1'),
+('24060120130059', 'Fathan Muhammad Rohmat', NULL, '2021', 'Komplek Tugu Barat', '087888327118', 'abdul@gmail.com', 'SNMPTN', '12', 'E1'),
+('24060120130152', 'Made Rohmat Subarjo', NULL, '2019', 'Komplek Jayabaya', '081315487465', 'made@gmail.com', 'SBMPTN', '13', 'E1');
 
 -- --------------------------------------------------------
 
