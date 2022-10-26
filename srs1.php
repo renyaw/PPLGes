4f17e9ca9998668c50b655c62065a8c8d66102c7
Fathan <74785057+Churma16@users.noreply.github.com>
2022-10-26 23:01:15 +0700

    styling srs1

--- srs1.php
+++ srs1.php
@@ -7,16 +7,24 @@
     <title>Update Data Pribadi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
     <script src="https://unpkg.com/htmlincludejs"></script>
+    <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
+    <style>
+      body {
+        font-family: "Inter";
+        font-size: 22px;
+      }
+      </style>
   </head>
-  <body>
+  <body class="bg-light">
     <?php
     //Cek apakah sudah login atau belum
     session_start();
     require_once "db_login.php";
     ?>
-    <div class="container px-5 pt-4 pb-5">
-      <h2>Data Mahasiswa</h2>
-      <div class="row">
+    <div class="container px-5 pt-4 pb-5 bg-white">
+      <h2 class="fw-bold">Data Mahasiswa</h2>
+      <div class="row border border-dark my-3 mx-1"></div>
+      <div class="row justify-content-evenly border px-2 py-5 mx-1 mt-3">
         <div class="col-4 mt-3">
           <div class="image overflow-hidden">
               <img class="" src="img\Bebek.png" alt="" width="250" />
@@ -71,6 +79,12 @@
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-    
+    <div class="bg-white">
+      <br>
+      <br>
+    </div>
+    <?PHP
+    include('Footer.php');
+    ?>
   </body>
 </html>
