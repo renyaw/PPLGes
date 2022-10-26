0e4e4928ec26fd4889cc5a23d85c28bb15b5bcc2
reikochn <norarenada@students.undip.ac.id>
2022-10-27 05:12:52 +0700

    Update srs1.php

--- srs1.php
+++ srs1.php
@@ -21,63 +21,73 @@
     session_start();
     require_once "db_login.php";
     ?>
-    <div class="container px-5 pt-4 pb-5 bg-white">
-      <h2 class="fw-bold">Data Mahasiswa</h2>
-      <div class="row border border-dark my-3 mx-1"></div>
-      <div class="row justify-content-evenly border px-2 py-5 mx-1 mt-3">
-        <div class="col-4 mt-3">
-          <div class="image overflow-hidden">
-              <img class="" src="img\Bebek.png" alt="" width="250" />
+    <div class="container mt-5">
+      <div class="card">
+        <div class="card-header fw-bold">
+          Data Mahasiswa
+        </div>
+        <div class="card-body">
+          
+        <div class="container px-5 pt-4 pb-5 bg-white">
+          <div class="row justify-content-evenly">
+            <div class="col-4 mt-3">
+              <div class="image overflow-hidden">
+                  <img class="" src="img\Bebek.png" alt="" width="250" />
+              </div>
+            </div>
+            <div class="col-6">
+            <form method="POST" name="form" autocomplete="on" action="srs1pt2.php">
+              <div class="mb-3 mt-3">
+                <input type="text" class="form-control" placeholder="NIM Mahasiswa" id="nim" name="nim">
+              </div>
+              <div class="mb-3 mt-5">
+                <input type="text" class="form-control" placeholder="Nama Mahasiswa" id="nama" name="nama">
+              </div>
+              <div class="row">
+              <div class=" col-6 mt-3">
+              <label for="angkatan" class="form-label">Angkatan:</label>
+                <select name="angkatan" id="angkatan" class="form-control form-control">
+                  <option value="">Pilih Angkatan</option>
+                  <option value="">2017</option>
+                  <option value="">2018</option>
+                  <option value="">2019</option>
+                  <option value="">2020</option>
+                  <option value="">2021</option>
+                  <option value="">2022</option>
+                  <option value="">2023</option>
+                </select>
+              </div>
+              <div class=" col-6 mt-3">
+              <label for="status" class="form-label">Status:</label>
+                <select name="status" id="status" class="form-control form-control">
+                    <option value="">Pilih Status</option>
+                    <option value="">Aktif</option>
+                    <option value="">Tidak Aktif</option>
+                    <option value="">Cuti</option>
+                    <option value="">Mangkir</option>
+                </select>
+              </div>
+              </div>
+              <div class="mb-3 mt-4">
+              <label for="wali" class="form-label">Kode Wali:</label>
+                <select name="kode_wali" id="kode_wali" class="form-control form-control">
+                <option value="">Pilih Kode Wali</option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                </select>
+              </div>
+              <button type="submit" class="btn btn-primary mt-4">Submit</button>
+            </form>
+            </div>
           </div>
         </div>
-        <div class="col-6">
-        <form method="POST" name="form" autocomplete="on" action="srs1pt2.php">
-          <div class="mb-3 mt-3">
-            <input type="text" class="form-control" placeholder="NIM Mahasiswa" id="nim" name="nim">
-          </div>
-          <div class="mb-3 mt-5">
-            <input type="text" class="form-control" placeholder="Nama Mahasiswa" id="nama" name="nama">
-          </div>
-          <div class="row">
-          <div class=" col-6 mt-3">
-          <label for="angkatan" class="form-label">Angkatan:</label>
-            <select name="angkatan" id="angkatan" class="form-control form-control">
-              <option value="">Pilih Angkatan</option>
-              <option value="">2017</option>
-              <option value="">2018</option>
-              <option value="">2019</option>
-              <option value="">2020</option>
-              <option value="">2021</option>
-              <option value="">2022</option>
-              <option value="">2023</option>
-            </select>
-          </div>
-          <div class=" col-6 mt-3">
-          <label for="status" class="form-label">Status:</label>
-            <select name="status" id="status" class="form-control form-control">
-                <option value="">Pilih Status</option>
-                <option value="">Aktif</option>
-                <option value="">Tidak Aktif</option>
-                <option value="">Cuti</option>
-                <option value="">Mangkir</option>
-            </select>
-          </div>
-          </div>
-          <div class="mb-3 mt-4">
-          <label for="wali" class="form-label">Kode Wali:</label>
-            <select name="kode_wali" id="kode_wali" class="form-control form-control">
-            <option value="">Pilih Kode Wali</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-            </select>
-          </div>
-          <button type="submit" class="btn btn-primary mt-4">Submit</button>
-        </form>
-        </div>
+        
+      </div>
       </div>
     </div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <div class="bg-white">
       <br>
