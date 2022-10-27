967eb28cc491c4c42246b387e86c9d194096faf9
Fathan <74785057+Churma16@users.noreply.github.com>
2022-10-27 18:42:39 +0700

    srs7 tolak terima irs

---
+++ srs7konfirmasiirs.php
@@ -0,0 +1,14 @@
+<?php
+require_once('db_login.php');
+
+$nim = $_GET['id'];
+
+$query = $db->query("INSERT into irs select * from irstemp where nim=$nim");
+$query2 = $db->query("DELETE from irstemp where nim=$nim");
+if($query){
+  header("location:srs7.php?pesan=sukses");
+}
+else{
+  header("location:srs7.php?pesan=gagal");
+}
+?>
