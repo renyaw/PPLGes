21a285cc16a1cfb1eb2d88d1f45bacad414a76e4
Fathan <74785057+Churma16@users.noreply.github.com>
2022-10-27 19:35:59 +0700

    edit srs7

--- srs7pkl.php
+++ srs7pkl.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>SRS 11</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
+    <style>
+      body {
+        font-family: "Inter";
+        font-size: 22px;
+      }
+      </style>
   </head>
   <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
@@ -50,7 +57,7 @@
           }
         }
         ?>
-      <h2 class="mb-5">Verifikasi PKL Mahasiswa</h2>
+      <h2 class="mb-5 fw-bold">Verifikasi PKL Mahasiswa</h2>
       <div class="row">
         <div class="col-2 mt-2">
           <div class="card border border-5">
