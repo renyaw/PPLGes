8f0fdeb4704d8a904357c2673f63a1e96496a6a1
Fathan <74785057+Churma16@users.noreply.github.com>
2022-10-27 19:27:43 +0700

    srs7 beserta anak anaknya

---
+++ srs7pklkonfirmasi.php
@@ -0,0 +1,25 @@
+<?php
+require_once('db_login.php');
+$action = $_GET['action'];
+$nim = $_GET['id'];
+
+if($action=="terima"){
+  $query = $db->query("INSERT into pkl select * from pkltemp where nim=$nim");
+  $query2 = $db->query("DELETE from pkltemp where nim=$nim");
+  if($query){
+    header("location:srs7pkl.php?pesan=sukses");
+  }
+  else{
+    header("location:srs7pkl.php?pesan=gagal");
+  }
+}
+else{
+  $query2 = $db->query("DELETE from pkltemp where nim=$nim");
+  if($query){
+    header("location:srs7pkl.php?pesan=suksestolak");
+  }
+  else{
+    header("location:srs7pkl.php?pesan=gagaltolak");
+  }
+}
+?>
