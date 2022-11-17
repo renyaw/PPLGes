824c9da011a7e790a44a8abea5c4a9c5fb114823
taskiaintan <111947053+taskiaintan@users.noreply.github.com>
2022-11-18 00:30:10 +0700

    srs 7

--- srs7.php
+++ srs7.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>SRS 11</title>
+    <title>SRS 7</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
     <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
     <script src="https://unpkg.com/htmlincludejs"></script>
