0fc0e5e2592b56d57e71cfd0946aacfbb4fb421a
Fathan <74785057+Churma16@users.noreply.github.com>
2022-12-04 14:31:57 +0700

    changing coba navbar dosen to navbar dosen

--- srs7.php
+++ srs7.php
@@ -16,7 +16,7 @@
   </head>
   <body class="bg-light">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <include src="cobanavbardosen.php"></include>
+    <include src="navbardosen.php"></include>
     <div class="container p-4 bg-white">
       <?php
         session_start();
