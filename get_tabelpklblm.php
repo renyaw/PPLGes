c06fc8ce0fe4842cbbbc7687736c27d9c05c7ff4
Fathan <74785057+Churma16@users.noreply.github.com>
2022-12-04 15:07:21 +0700

    removing pagination

--- get_tabelpklblm.php
+++ get_tabelpklblm.php
@@ -14,7 +14,7 @@ else{
 
 $jml= mysqli_num_rows($result);
 
-$batas = 10;
+$batas = 5;
 $halaman = isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
 $halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;	
 
@@ -49,20 +49,4 @@ else{
 }
 ?>
 
-<nav>
-			<ul class="pagination justify-content-center">
-				<li class="page-item">
-					<a class="page-link" <?php if($halaman > 1){ echo "href='?halaman=$previous'"; } ?>>Previous</a>
-				</li>
-				<?php 
-				for($x=1;$x<=$total_halaman;$x++){
-					?> 
-					<li class="page-item"><a class="page-link" href="?halaman=<?php echo $x ?>"><?php echo $x; ?></a></li>
-					<?php
-				}
-				?>				
-				<li class="page-item">
-					<a  class="page-link" <?php if($halaman < $total_halaman) { echo "href='?halaman=$next'"; } ?>>Next</a>
-				</li>
-			</ul>
-		</nav>
+
--- srs14_blm.php
+++ srs14_blm.php
@@ -49,23 +49,6 @@
       </table>
     </div>
       <br />
-      <nav aria-label="Page navigation example">
-        <ul class="pagination justify-content-center">
-          <li class="page-item disabled">
-            <a class="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
       <button type="button"  class="btn btn-warning mt-4 shadow" onclick="location.href='srs12.php'">Kembali</button>
     </div>
     
